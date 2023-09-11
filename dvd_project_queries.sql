2fa92691e3b888235890e91cf6bad2e5c0f74b60
Chungon <chungon.tse@gmail.com>
2023-09-11 14:34:48 -0400

    commented sql

--- dvd_project_queries.sql
+++ dvd_project_queries.sql
@@ -1,11 +1,11 @@
--- 1.
+-- 1. The cast of Chamber Italian
 SELECT actor.actor_id, CONCAT(actor.first_name, ' ', actor.last_name) AS actor_full_name
 FROM film
 JOIN film_actor ON film.film_id = film_actor.film_id
 JOIN actor ON film_actor.actor_id = actor.actor_id
 WHERE film.film_id = 133;
 
--- 2.
+-- 2. Alec Wayne's films
 SELECT film.film_id, film.title, language.name AS film_language, film.release_year
 FROM film
 JOIN film_actor ON film.film_id = film_actor.film_id
@@ -14,7 +14,7 @@ JOIN language ON film.language_id = language.language_id
 WHERE actor.actor_id = 29
 ORDER BY film.title ASC;
 
--- 3a.
+-- 3a. Customers by country, store 1
 SELECT COUNT(*) AS number_of_customers, c.country, cust.store_id
 FROM country c
 JOIN city ON c.country_id = city.country_id
@@ -23,7 +23,7 @@ JOIN customer cust ON a.address_id = cust.address_id
 WHERE cust.store_id = 1
 GROUP BY c.country, cust.store_id
 ORDER BY number_of_customers DESC;
---3b.
+--3b. Customers by country, store 2
 SELECT COUNT(*) AS number_of_customers, c.country, cust.store_id
 FROM country c
 JOIN city ON c.country_id = city.country_id
@@ -33,7 +33,7 @@ WHERE cust.store_id = 2
 GROUP BY c.country, cust.store_id
 ORDER BY number_of_customers DESC;
 
---4
+--4. Customers by payment amount
 WITH total_payment_by_customer AS (
 SELECT sum(amount) as payments, customer_id 
 from payment
@@ -44,12 +44,12 @@ FROM total_payment_by_customer cp
 JOIN customer c ON cp.customer_id = c.customer_id
 order by payments desc;
 
---5
+--5. Horror films not having deleted scenes
 select count(*)
 from film f
 JOIN film_category fc ON f.film_id = fc.film_id
 JOIN category cat ON fc.category_id = cat.category_id
-WHERE cat.name = 'Horror'
+WHERE cat.name = 'Horror';
 
 SELECT f.film_id, f.title, f.special_features
 FROM film f
